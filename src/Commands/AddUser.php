bf748b16ddb907941f0c54396fb188292cb74488
Amos Shacham <amos@boaideas.com>
2017-08-21 11:35:11 +0300

    initial

---
+++ src/Commands/AddUser.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BOAIdeas\AddUser\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+
+class AddUser extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new laravel user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->ask('User name');
+        $email = $this->ask('User email');
+        $password = $this->ask('User password');
+
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            ]);
+        $this->info('New user created!');
+    }
+
+}
